f0919c7fbb5ee04ef6529355a9fb5bdbd7e8bcf6
Paramesh612 <136327127+Paramesh612@users.noreply.github.com>
2024-10-03 13:57:16 +0530

    Tried some frontend

---
+++ Code/FrontEnd Try/Test1.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.awt.*;
+public class Test1 {
+    private JFrame frame ;
+    private JTextField tf1,tf2;
+    private JLabel tl1,tl2;
+
+    Test1(){
+        frame = new JFrame();
+        frame.setSize(400,400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // frame.doLayout();
+        frame.setLayout(new FlowLayout());
+
+        tf1 = new JTextField(20);
+        tf2 = new JTextField(20);
+        tl1 = new JLabel("Label 1: ");
+        tl2 = new JLabel("Label 2: ");
+
+        frame.add(tl1);
+        frame.add(tf1);
+
+        frame.add(tl2);
+        frame.add(tf2);
+
+
+        frame.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        Test1 test = new Test1();
+    }
+}
