959336a5d9afed3439161878edea7ce956c50758
Paramesh612 <136327127+Paramesh612@users.noreply.github.com>
2024-11-07 14:17:42 +0530

    Canva link
    https://www.canva.com/design/DAGVuN65M88/geZWENWtr94CNL58ofpl3g/edit?utm_content=DAGVuN65M88&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton

---
+++ Code Latest/src/Login_GUI.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Login_GUI {
+    Login_GUI(){
+        JFrame frame = new JFrame();
+        frame.setSize(600,600);
+
+        JLabel 
+        JTextField uname = new JTextField(10);
+
+
+        frame.setVisible(true);
+    }
+}
