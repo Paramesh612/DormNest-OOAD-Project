bbc5eccc0fe468e329f249b23f7a8d2bd6f3ec94
Paramesh612 <136327127+Paramesh612@users.noreply.github.com>
2024-11-11 20:45:31 +0530

    Update Main_try_DB.java

--- Code Latest/src/Main_try_DB.java
+++ Code Latest/src/Main_try_DB.java
@@ -4,6 +4,6 @@ public class Main_try_DB {
     public static void main(String[] args) {
         DB_Functions db = new DB_Functions();
         // Connection conn = db.connect_to_db("TestDB", "postgres", "root");
-        db.createTable(conn, "TobleIsTable");
+        // db.createTable(conn, "TobleIsTable");
     }
 }
